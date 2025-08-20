7a0a5e88bceb405ff35dc1d64babaf9dce0fe2d9
roanbaluyut <roanbaluyut10.2@gmail.com>
2025-08-20 09:48:24 +0800

    feat: implement operators and expressions
    - Add mathematical operators (+, -, *, /, **)
    - Implement assignment operators (+=, -=, *=, /=, ++, --)
    - Create comparison operators for boolean logic
    - Understand operator precedence and parentheses
    - Complete BMI Calculator coding challenge with real-world application

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,60 +1,180 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
-let js = "amazing";
-console.log(40 + 8 + 23 -10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 -10);
 
-console.log("=== VARIABLES ===");
 
-let firstName = "Jonas";
-console.log(firstName);
+// First Part
+// console.log("=== VARIABLES ===");
 
-let age = 30;
-age = 31;
-console.log(age);
+// let firstName = "Jonas";
+// console.log(firstName);
 
-const birthYear = 1991;
-console.log(birthYear);
+// let age = 30;
+// age = 31;
+// console.log(age);
 
-const PI = 3.1415;
-console.log(PI);
+// const birthYear = 1991;
+// console.log(birthYear);
 
-var job = "programmer";
-job = "teacher";
-console.log(job);
+// const PI = 3.1415;
+// console.log(PI);
 
-let lastName = "Sarah";
-let myCurrentJob = "teacher";
-let PIZ = 3.14;
+// var job = "programmer";
+// job = "teacher";
+// console.log(job);
 
-const country = "Portugal";
-const language = "Portugese";
-const population = 10;
+// let lastName = "Sarah";
+// let myCurrentJob = "teacher";
+// let PIZ = 3.14;
 
-let ageNow = 25;
-ageNow = 26;
+// const country = "Portugal";
+// const language = "Portugese";
+// const population = 10;
 
-console.log("=== DATA TYPES ===");
-let age1 = 23;
-console.log(age);
-console.log(typeof age1);
+// let ageNow = 25;
+// ageNow = 26;
 
-let firstName1 = "Jonas";
-console.log(firstName1);
-console.log(typeof firstName1);
+// console.log("=== DATA TYPES ===");
+// let age1 = 23;
+// console.log(age);
+// console.log(typeof age1);
 
-let javascriptIsFun = true;
-console.log(javascriptIsFun);
-console.log(typeof javascriptIsFun);
+// let firstName1 = "Jonas";
+// console.log(firstName1);
+// console.log(typeof firstName1);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-let dynamicVariable = 23;
-console.log(dynamicVariable, typeof dynamicVariable);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-dynamicVariable = "Now I'm a string!";
-console.log(dynamicVariable, typeof dynamicVariable);
+// let dynamicVariable = 23;
+// console.log(dynamicVariable, typeof dynamicVariable);
 
-dynamicVariable = true;
-console.log(dynamicVariable, typeof dynamicVariable);
+// dynamicVariable = "Now I'm a string!";
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+// dynamicVariable = true;
+// console.log(dynamicVariable, typeof dynamicVariable);
+
+
+// Second Part
+// Basic Operators
+// console.log("=== BASIC OPERATORS ===");
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+// console.log("Math operations;");
+// console.log("Addition: ", 10 + 5);
+// console.log("Subtraction: ", 20 - 8);
+// console.log("Multipplication: ", 4 * 7);
+// console.log("Division: ", 15/3);
+// console.log("Experimentation: ", 2 ** 3);
+
+
+// //String Concat
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// console.log("Hello " + "World" + "!");
+// console.log("I am " + 25 + " years old.");
+
+// // Assignment Operators
+// console.log("=== ASSIGNMENT OPERATORS ===");
+
+// let x = 10 + 5;
+// console.log("x starts as:", x);
+
+// x += 10;
+// console.log("After x += 10:", x);
+
+// x *= 4;
+// console.log("After x*= 4:", x);
+
+// x /= 2;
+// console.log("After x /= 2:", 2);
+
+// x ++; 
+// console.log("After x++:", x);
+
+// x --;
+// x --;
+// console.log("After x-- twice:", x);
+
+
+// // Comparison Operators
+// console.log("=== COMPARISON OPERATORS");
+
+// console.log("Age comparison");
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah >= 18);
+// console.log(ageJonas < 30);
+
+// console.log("Number comparisons:");
+// console.log(25 > 20);
+// console.log(15 < 10);
+// console.log(18 >= 18);
+// console.log(16 <= 15);
+
+
+// const isFullAge1 = ageSarah >= 18;
+// console.log("Sarah is adult:", isFullAge1);
+
+// const isJonasOlder = ageJonas > ageSarah;
+// console.log("Jonas is older:", isJonasOlder);
+
+
+// console.log("Complex comparison:");
+// console.log(now - 1991 > now - 2018);
+// console.log(ageJonas > ageSarah);
+
+
+// Operator Precedence
+console.log("=== Operator Precedence ===");
+
+const now = 2037;
+const ageJonas = now -1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x,y;
+x = y = 25 - 10 - 5;
+console.log(x,y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+
+
+// Your code here:
+// 1. Calculate BMIs
+// 2. Create markHigherBMI variable
+// 3. Log results to console
+
+// Test Data 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const bmiMark = massMark / heightMark ** 2;
+const bmiMark2 = massMark / (heightMark * heightMark);
+
+const bmiJohn = massJohn / heightJohn ** 2;
+const bmiJohn2 = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = bmiMark > bmiJohn;
+console.log("Mark's bmi is higher than John:" + markHigherBMI);
+
+
+
